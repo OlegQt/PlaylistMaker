44c27397fb082d2ec75ba791ff62d32812f30bfe
OlegQt <xqjh@mail.ru>
2023-03-22 23:27:18 +0300

    Intent для кнопки "поделиться приложением"

--- app/src/main/java/com/playlistmaker/ActivitySettings.kt
+++ app/src/main/java/com/playlistmaker/ActivitySettings.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 
 class ActivitySettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,17 @@ class ActivitySettings : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val imgBack: ImageView = findViewById(R.id.backToMainActivity)
-        //val mainScreen = Intent(this.baseContext, MainActivity::class.java)
+        val imgShare:ImageView = findViewById(R.id.img_share)
+
 
         imgBack.setOnClickListener{finish()}
+        imgShare.setOnClickListener{
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_yandex))
+            shareIntent.type = getString(R.string.share_type);
+            startActivity(shareIntent)
+            //Toast.makeText(this.baseContext,"xt",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
