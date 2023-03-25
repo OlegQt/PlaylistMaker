848c307ca5c6b17d0315ad970643fcf18ce2d531
OlegQt <xqjh@mail.ru>
2023-03-25 23:11:48 +0300

    Функционал Restore and Save

--- app/src/main/java/com/playlistmaker/ActivitySearch.kt
+++ app/src/main/java/com/playlistmaker/ActivitySearch.kt
@@ -1,29 +1,41 @@
 package com.playlistmaker
 
+import android.app.Activity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivitySearch : AppCompatActivity() {
+    private val tag: String = "LOGTest"
+    private var strSearch: String = String()
+    private var txtSearch: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        // Переменные для элементов интерфейса
         val btnBack: ImageView = findViewById(R.id.btnBack)
         val btnCls: ImageView = findViewById(R.id.cls_search)
-        val txtSearch: EditText = findViewById(R.id.txt_search)
-
-        val txtSearchWatcher = object :TextWatcher{
+        txtSearch = findViewById(R.id.txt_search)
+        // Анонимный
+        val txtSearchWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 //
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.isNullOrEmpty()) btnCls.visibility=View.GONE
-                else btnCls.visibility=View.VISIBLE
+                if (s.isNullOrEmpty()) btnCls.visibility = View.GONE
+                else {
+                    btnCls.visibility = View.VISIBLE
+                    strSearch = s.toString()
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -31,13 +43,37 @@ class ActivitySearch : AppCompatActivity() {
             }
 
         }
-
+        // Вешаем слушателей на элелементы
         btnBack.setOnClickListener { finish() }
-        txtSearch.addTextChangedListener(txtSearchWatcher)
-        btnCls.setOnClickListener{
-            txtSearch.setText("")
+        txtSearch?.addTextChangedListener(txtSearchWatcher)
+        btnCls.setOnClickListener {
+            txtSearch?.setText("") // Очиста текстового поля
+            this.strSearch =
+                "" // Очистка строки, иначе даже при стертом значении текстого поля, после поворота экрана, значение восстановиться
+            // Убираем клавиатуру
+            val inputMethodManager =
+                getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(txtSearch?.windowToken, 0)
+            // Лог для проверки
+            Log.d(tag, "Txt Clear")
         }
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("searchTxt", this.strSearch)
+        Log.d(tag, "onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        this.strSearch = savedInstanceState.getString("searchTxt").toString()
+        Log.d(tag, "onRestoreInstanceState")
+        // Проверка работоспособности
+        //Toast.makeText(this,this.strSearch,Toast.LENGTH_SHORT).show()
+        this.txtSearch?.setText(this.strSearch)
+        // Как записать strSearch в txtSearch???
 
     }
 }
