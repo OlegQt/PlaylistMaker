e90679692a18b84bbd2d6cbcfb966c1697d1ab79
OlegQt <xqjh@mail.ru>
2023-04-05 18:46:46 +0300

    Glide connected. Loading image completed

--- app/src/main/java/com/playlistmaker/Logic/SearchTrackAdapter.kt
+++ app/src/main/java/com/playlistmaker/Logic/SearchTrackAdapter.kt
@@ -7,6 +7,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.playlistmaker.R
 
 class SearchTrackAdapter(private val trackList: ArrayList<Track>) :
@@ -35,17 +37,25 @@ class SearchTrackViewHolder(private val item: View) : ViewHolder(item) {
     private var txtArtistName: TextView? = null
     private var txtTrackTime: TextView? = null
     private var imgArtCover: ImageView? = null
+
     init {
         txtTrackName = item.findViewById(R.id.track_name)
-        txtArtistName= item.findViewById(R.id.artist_name)
-        txtTrackTime =item.findViewById(R.id.track_time)
+        txtArtistName = item.findViewById(R.id.artist_name)
+        txtTrackTime = item.findViewById(R.id.track_time)
         imgArtCover = item.findViewById(R.id.art_work)
     }
 
     fun bind(musTrack: Track) {
         txtTrackName?.text = musTrack.trackName
         txtArtistName?.text = musTrack.artistName
-        txtTrackTime?.text=musTrack.trackTime
-    }
+        txtTrackTime?.text = musTrack.trackTime
 
+        if (imgArtCover != null) Glide
+            .with(item.context)
+            .load(musTrack.artworkUrl100)
+            .placeholder(R.drawable.brain)
+            .centerCrop()
+            .transform(RoundedCorners(2))
+            .into(imgArtCover!!)
+    }
 }
