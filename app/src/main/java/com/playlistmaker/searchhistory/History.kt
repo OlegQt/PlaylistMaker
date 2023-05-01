48e690301e06d36ef8487f4712e605867e5b5cf8
Oleg <xqjh@mail.ru>
2023-05-01 10:30:03 +0300

    Треки добавляются корректно в историю и отображаются во временной  TextView

--- app/src/main/java/com/playlistmaker/searchhistory/History.kt
+++ app/src/main/java/com/playlistmaker/searchhistory/History.kt
@@ -1,5 +1,6 @@
 package com.playlistmaker.searchhistory
 
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import com.playlistmaker.ActivitySearch
@@ -8,24 +9,48 @@ import com.playlistmaker.R
 import com.playlistmaker.Theme.App
 
 class History {
-    val trackHistoryList:MutableSet<Track> = mutableSetOf()
-    lateinit var txtHistory:TextView
+    val trackHistoryList: MutableList<Track> = mutableListOf()
+    lateinit var txtHistory: TextView
 
-    fun deployExtraUi(activity:ActivitySearch){
+    fun deployExtraUi(activity: ActivitySearch) {
         txtHistory = activity.findViewById<TextView>(R.id.history)
     }
 
-    fun addToSearchHistory(track: Track){
-        if(!trackHistoryList.add(track)) Toast.makeText(App.instance.baseContext,
-        "Already in history",Toast.LENGTH_SHORT).show()
+    fun addToSearchHistory(track: Track) {
+        // Search if track is already exists, returns track index in list
+        // Return -1 if no such element was found.
+        var res = trackHistoryList.indexOf(track)
+
+        // If track is new fo historyList (res == null)
+        if (res != -1) {
+            // Toast.makeText(App.instance.baseContext, "Already in history ", Toast.LENGTH_SHORT).show()
+            // Ниже реализовал смещение трека на первую позицию
+            val temporaryTrack = trackHistoryList[res]
+            trackHistoryList.removeAt(res)
+            trackHistoryList.add(0, temporaryTrack)
+
+        } else {
+            // Добавляем трэки в начало
+            // Если треков больше 10 удаляем заключительный
+            trackHistoryList.add(0, track)
+            if (trackHistoryList.size > 10) trackHistoryList.removeLast()
+        }
+
+        // Show history in View
         showAllSearchHistory()
     }
 
-    fun showAllSearchHistory(){
-        val sb:StringBuilder = java.lang.StringBuilder()
+    private fun showAllSearchHistory() {
+        this.txtHistory.visibility = View.VISIBLE
+        val sb: StringBuilder = java.lang.StringBuilder()
+        var iter = 0;
         this.trackHistoryList.forEach {
+            sb.append((iter++).toString())
             sb.append(it.trackName)
+            sb.append("\n")
         }
         this.txtHistory.text = sb.toString()
     }
+
+
 }
