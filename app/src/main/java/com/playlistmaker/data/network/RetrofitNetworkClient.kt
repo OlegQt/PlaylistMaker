66db139cf0cf9d1966e0128e69b61bab32430cb0
OlegWork <xqjh@mail.ru>
2023-07-13 15:08:03 +0300

    Проверка для Sealed interface в RetrofitNetworkClient  изменена на when В ActivitySearchVm музыкальный запрос создается напрямую без Creator

--- app/src/main/java/com/playlistmaker/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/playlistmaker/data/network/RetrofitNetworkClient.kt
@@ -6,7 +6,7 @@ import com.playlistmaker.domain.models.SearchRequest
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitNetworkClient :NetworkClient{
+class RetrofitNetworkClient : NetworkClient {
 
     private val baseUrl = "https://itunes.apple.com"
 
@@ -20,13 +20,14 @@ class RetrofitNetworkClient :NetworkClient{
     private val mediaApi: ItunesMediaSearchApi = retrofit.create(ItunesMediaSearchApi::class.java)
 
     override fun doRequest(request: Any): MusicResponse {
-        return if(request is SearchRequest.MusicSearchRequest){
-            val response = mediaApi.searchMusic(request.searchParam).execute()
-            val body = response.body() ?: MusicResponse()
-            body.resultCode = response.code()
-            body
-        } else{
-            MusicResponse()
+        when (request) {
+            is SearchRequest.MusicSearchRequest -> {
+                val response = mediaApi.searchMusic(request.searchParam).execute()
+                val body = response.body() ?: MusicResponse()
+                body.resultCode = response.code()
+                return body
+            }
         }
+        return MusicResponse()
     }
 }
