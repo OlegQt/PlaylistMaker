01177e8a8659bbbc70f554c8ea09906c24ba5ba5
OlegHome <xqjh@mail.ru>
2023-08-09 22:00:46 +0300

    Реализовано управление фрагментами через supportManager при нажатии по навигационной панели

--- app/src/main/java/com/playlistmaker/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/playlistmaker/presentation/ui/activities/MainActivity.kt
@@ -2,16 +2,23 @@ package com.playlistmaker.presentation.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.playlistmaker.R
+import com.playlistmaker.databinding.ActivityMainBinding
 import com.playlistmaker.presentation.ui.fragments.MediaLibraryFragment
+import com.playlistmaker.presentation.ui.fragments.SearchFragment
+import com.playlistmaker.presentation.ui.fragments.SettingsFragment
 import com.playlistmaker.presentation.ui.viewmodel.ActivityMainVm
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //val btnMedia = findViewById<Button>(R.id.media)
         //val btnSearch = findViewById<Button>(R.id.search)
@@ -19,11 +26,22 @@ class MainActivity : AppCompatActivity() {
 
         val vm: ActivityMainVm by viewModel()
 
+        binding.rootNavigationBar.setOnItemSelectedListener{
+            var temporalFragment:Fragment? = null
+            when(it.itemId){
+                R.id.pageA -> temporalFragment=SearchFragment()
+                R.id.PageB ->temporalFragment=MediaLibraryFragment()
+                R.id.PageC -> temporalFragment = SettingsFragment()
+            }
+            this.manualFragmentOpening(temporalFragment)
+        }
+
         if(savedInstanceState==null){
             supportFragmentManager.commit {
                 add(R.id.root_placeholder, MediaLibraryFragment())
             }
         }
+
 
 /*        vm.screen.observe(this) {
             val intent = when (it) {
@@ -48,5 +66,15 @@ class MainActivity : AppCompatActivity() {
         //btnSearch.setOnClickListener { vm.loadAnotherActivity(Screen.SEARCH.screenName) }
         //btnMedia.setOnClickListener { vm.loadAnotherActivity(Screen.MEDIA.screenName) }
         //btnSettings.setOnClickListener { vm.loadAnotherActivity(Screen.SETTINGS.screenName) }
+    }
+
+    private fun manualFragmentOpening(internalFragment: Fragment?):Boolean{
+        return if (internalFragment!=null) {
+            supportFragmentManager.commit {
+                replace(R.id.root_placeholder, internalFragment)
+                addToBackStack(null)
+            }
+            true
+        } else false
     }
 }
