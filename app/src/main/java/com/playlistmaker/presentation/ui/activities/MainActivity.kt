47d8778db090ca498c4282601c4f3cd395ef01da
OlegHome <xqjh@mail.ru>
2023-08-12 09:54:54 +0300

    fixed switch_style color in fragment_settings

--- app/src/main/java/com/playlistmaker/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/playlistmaker/presentation/ui/activities/MainActivity.kt
@@ -6,22 +6,33 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.playlistmaker.R
 import com.playlistmaker.databinding.ActivityMainBinding
+import com.playlistmaker.presentation.models.AlertMessaging
 import com.playlistmaker.presentation.ui.viewmodel.ActivityMainVm
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+class MainActivity : AppCompatActivity(), AlertMessaging {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.root_placeholder) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.root_placeholder) as NavHostFragment
         binding.rootNavigationBar.setupWithNavController(navController = navHostFragment.navController)
 
 
+    }
+
+    override fun showAlertDialog(alertMessage: String) {
+        MaterialAlertDialogBuilder(this)
+            .setMessage(alertMessage)
+            .setTitle("Dialog")
+            .setNeutralButton("OK", null)
+            .show()
     }
 }
