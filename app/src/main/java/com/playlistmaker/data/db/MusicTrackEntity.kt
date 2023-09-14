0280961087046c058a54acb7ee6066e82fc96e22
Magister <xqjh@mail.ru>
2023-09-14 09:13:50 +0300

    MusicTrackEntity расширен проверка VCS

--- app/src/main/java/com/playlistmaker/data/db/MusicTrackEntity.kt
+++ app/src/main/java/com/playlistmaker/data/db/MusicTrackEntity.kt
@@ -1,10 +1,21 @@
 package com.playlistmaker.data.db
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "FavouriteTracks")
 data class MusicTrackEntity(
         @PrimaryKey
-        val id:Int
+        val id:Long,
+        @ColumnInfo(name = "track_name")
+        val trackName: String,
+        val artistName: String,
+        val trackTimeMillis: Long,
+        val artworkUrl100: String,
+        val collectionName: String,
+        val releaseDate: String,
+        val primaryGenreName: String,
+        val country: String,
+        val previewUrl: String
 )
